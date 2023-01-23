a7a25d8fd600ce93f5e67daf2ec3765459633159
MeewNikolajP <nikolaj.pregaard@meew.dk>
2023-01-23 14:28:26 +0100

    feat: Profile List functional, shows list of users

--- src/ui/components/frontpage/profile-list/ProfileList.tsx
+++ src/ui/components/frontpage/profile-list/ProfileList.tsx
@@ -1,53 +1,45 @@
 import { IonCol, IonRow } from '@ionic/react';
 import { supabase } from 'apis/supabaseClient';
+import { useEffect, useState } from 'react';
+import { Users } from 'types/profilePageInfo';
 
 const ProfileList: React.FC = () => {
+  const [users, setUsers] = useState<Users[] | null>([]);
+
+  const getUsers = async () => {
+    const { error, data } = await supabase.from('users').select();
+
+    if (data) {
+      setUsers(data);
+    }
+
+    if (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <IonRow>
       <IonCol>
         <div className="grid grid-cols-1 divide-y">
-          <div className='flex flex-row'>
-            <div className='pl-6 pt-4 pr-4 pb-4'>
-              <div className="relative w-16 h-16 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600"> {/* circle around icon */}
-                <img src="https://www.w3schools.com/css/paris.jpg" alt="" style={{ height: '68px', width: '68px' }}></img>
+          {users?.map((user) => {
+            return (
+              <div key={user.id} className="flex flex-row">
+                <div className="pl-6 pt-4 pr-4 pb-4">
+                  <div className="relative w-16 h-16 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                    <img src="https://www.w3schools.com/css/paris.jpg" alt="" style={{ height: '68px', width: '68px' }}></img>
+                  </div>
+                </div>
+                <p className="pt-[33px] text-lg">{user.fullname}</p>
               </div>
-            </div>
-            <p className='pt-[33px] text-lg'>Nikolaj Engstrøm Pregaard</p>
-          </div>
-          <div className='flex flex-row'>
-            <div className='pl-6 pt-4 pr-4 pb-4'>
-              <div className="relative w-16 h-16 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600"> {/* circle around icon */}
-                <img src="https://www.w3schools.com/css/paris.jpg" alt="" style={{ height: '68px', width: '68px' }}></img>
-              </div>
-            </div>
-            <p className='pt-[33px] text-lg'>Nikolaj Engstrøm Pregaard</p>
-          </div>
-          <div className='flex flex-row'>
-            <div className='pl-6 pt-4 pr-4 pb-4'>
-              <div className="relative w-16 h-16 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600"> {/* circle around icon */}
-                <img src="https://www.w3schools.com/css/paris.jpg" alt="" style={{ height: '68px', width: '68px' }}></img>
-              </div>
-            </div>
-            <p className='pt-[33px] text-lg'>Nikolaj Engstrøm Pregaard</p>
-          </div>
-          <div className='flex flex-row'>
-            <div className='pl-6 pt-4 pr-4 pb-4'>
-              <div className="relative w-16 h-16 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600"> {/* circle around icon */}
-                <img src="https://www.w3schools.com/css/paris.jpg" alt="" style={{ height: '68px', width: '68px' }}></img>
-              </div>
-            </div>
-            <p className='pt-[33px] text-lg'>Nikolaj Engstrøm Pregaard</p>
-          </div>
-          <div className='flex flex-row'>
-            <div className='pl-6 pt-4 pr-4 pb-4'>
-              <div className="relative w-16 h-16 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600"> {/* circle around icon */}
-                <img src="https://www.w3schools.com/css/paris.jpg" alt="" style={{ height: '68px', width: '68px' }}></img>
-              </div>
-            </div>
-            <p className='pt-[33px] text-lg'>Nikolaj Engstrøm Pregaard</p>
-          </div> 
+            );
+          })}
         </div>
-        </IonCol>
+      </IonCol>
     </IonRow>
   );
 };
