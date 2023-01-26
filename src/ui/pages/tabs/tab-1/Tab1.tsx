d36c343c880bfd900595f33bfd40609e23ba9b11
MeewNikolajP <nikolaj.pregaard@meew.dk>
2023-01-26 12:00:17 +0100

    feat: edge function displays name on tab1

--- src/ui/pages/tabs/tab-1/Tab1.tsx
+++ src/ui/pages/tabs/tab-1/Tab1.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IonContent, IonTitle } from '@ionic/react';
 import TakePicture from 'ui/components/frontpage/take-picture/TakePicture';
 import { Photo } from '@capacitor/camera';
+import { supabase } from 'apis/supabaseClient';
+import { useloggedInUserStore } from 'store/loggedInUser';
 
 const Tab1: React.FC = () => {
   const [picture, setPicture] = React.useState<Photo>();
+  const [message, setMessage] = useState<string>('');
+  const user = useloggedInUserStore();
+
+  useEffect(() => {
+    supabase.functions
+      .invoke('hello_world', {
+        body: { name: user.loggedInUser?.fullname },
+      })
+      .then((res) => setMessage(res.data.message));
+  }, [user]);
 
   return (
     <IonContent color={'white-background'}>
-      {!picture && <IonTitle>Take a photo!</IonTitle>}
+      <h3 className="justify-center flex h-screen w-screen items-center "> {message} </h3>
       {picture && <img src={picture?.webPath} alt="your upload" className="h-full w-auto m-auto" />}
 
       <TakePicture onPictureTaken={(p) => setPicture(p)} />
